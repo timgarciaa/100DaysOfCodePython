85c3a56be10d158072f9e084199a843b4e2c167d
Tim Garcia <timgarciaa@gmail.com>
2023-06-12 14:15:45 +0800

    Day 1 examples and notes

---
+++ Day1/Examples.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this")
+print("print('what to print')")
+#string manipulation
+print("Hello world! \n Hello world!")
+print("Hello " + "Angela")
+print("String concatenation is done with the \"+\" sign.")
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?") + "!")
+print(len(input("What is your name?")))
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+
+print("a: " + a)
+print("b: " + b)
