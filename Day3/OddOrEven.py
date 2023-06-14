2113f33b44a614d649bb4cb1181bdb21e1d40194
Tim Garcia <timgarciaa@gmail.com>
2023-06-14 08:35:10 +0800

    Day 3 Odd or Even Exercise

---
+++ Day3/OddOrEven.py
@@ -0,0 +1,11 @@
+# Don't change the code below
+number = int(input("Which number do you want to check? "))
+# Don't change the code above
+
+#Write your code below this line
+remainder = number % 2
+
+if remainder == 1:
+    print("This is an odd number")
+else:
+    print("This is an even number")
