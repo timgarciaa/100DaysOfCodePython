87dce7f1d40f52c1e00b86c2dd00607f5a40b995
tim <timgarciaa@gmail.com>
2023-06-20 11:46:56 +0800

    Day 6 Indentation and functions

---
+++ Day6/Main.py
@@ -0,0 +1,16 @@
+def my_function():
+    print("Hello")
+    print("bye")
+
+my_function()
+
+fruits = ["apple", "orange", "grapes"]
+
+fruit = ""
+x = 0
+
+while fruit != "apple":
+    fruit = fruits[x]
+    x += 1
+
+print(fruit)
